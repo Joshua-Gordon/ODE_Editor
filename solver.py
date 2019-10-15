18c723e1d9226cb558e0760a217492f0c610663d
Joshua-Gordon <joshua.sam.gordon@gmail.com>
2019-10-15 01:05:24 -0400

    Export and import JSON

--- solver.py
+++ solver.py
@@ -2,6 +2,8 @@ import collections
 from scipy.integrate import solve_ivp
 from numpy import *
 
+import json
+
 class Solver(object):
     def __init__(self):
         self.equations = collections.OrderedDict()
@@ -35,6 +37,16 @@ def model(t,Y):
             solution = solve_ivp(model,[self.start,self.stop],list(self.conditions.values()))
         return solution
 
+    def toJSON(self):
+        return json.dumps(vars(self))
+
+    def fromJSON(self,txt):
+        js = json.loads(txt)
+        self.equations = collections.OrderedDict(js["equations"])
+        self.conditions = collections.OrderedDict(js["conditions"])
+        self.start = float(js["start"])
+        self.stop = float(js["stop"])
+
     def __repr__(self):
         return f'<Solver eq={self.equations!r} co={self.conditions!r}>'
 
@@ -48,3 +60,9 @@ def test_solver():
     s.conditions["omega"] = 0
     sol = s.solve()
     #sol = s.solve(progress_cb=print)
+    print(s.toJSON())
+    j = s.toJSON()
+    t = Solver()
+    t.fromJSON(j)
+    print(t.solve())
+    print(s.solve())
